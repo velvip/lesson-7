6520e3f92cda164359501857f4f4f1a561c5814d
Velbl4 <velvip1@gmail.com>
2021-09-03 20:36:17 +0300

    ip and test

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const express = require('express');
 
 // константы
 const port = 8080;
-const host = '0.0.0.0';
+const host = '104.45.46.248';
 
 // приложение
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World by Vel 2');
+  res.send('TEST');
 });
 
 app.listen(port, host);
-console.log(`running on http://104.45.46.248:${port}`);
+console.log(`running on http://${host}:${port}`);
