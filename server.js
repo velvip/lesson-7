94f1dc4a8e6496a5539181a6f215e8305cff029d
Velbl4 <velvip1@gmail.com>
2021-09-07 18:10:13 +0300

    test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const host = '0.0.0.0';
 // приложение
 const app = express();
 app.get('/', (req, res) => {
-  res.send('TEST NEW File');
+  res.send('Test For all');
 });
 
 app.listen(port, host);
