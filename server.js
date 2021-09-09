86d6e711b5f0334fbe73fd9c6cf4c869fd93e297
velbl4 <57460303+velvip@users.noreply.github.com>
2021-09-09 16:28:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const host = '0.0.0.0';
 // приложение
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Test For all');
+  res.send('Test 09.09.2021');
 });
 
 app.listen(port, host);
